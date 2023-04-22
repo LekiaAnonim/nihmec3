e16d0e3254c44c4fd0e00d3ff066416d4b7688fc
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-04-22 22:24:56 +0100

    remove Docker

--- nihmec/settings/base.py
+++ nihmec/settings/base.py
@@ -119,12 +119,11 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
-AWS_LOCATION = 'static'
 
 
 
