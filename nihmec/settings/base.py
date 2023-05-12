6e36043ef74bcfa7aaf7ef14f6e55b19474731ca
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-05-12 23:06:09 +0100

    change staticfiles_dirs

--- nihmec/settings/base.py
+++ nihmec/settings/base.py
@@ -211,7 +211,7 @@ STATICFILES_FINDERS = [
 # See https://docs.djangoproject.com/en/4.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "nihmec/static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
