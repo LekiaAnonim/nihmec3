c2fbf81dfacb3afc1ac4e56d42c95345d6dc06cb
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-05-15 13:39:57 +0100

    change model structure for preconference, and sponsorship

--- nihmec/settings/dev.py
+++ nihmec/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 from django.core.management.utils import get_random_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
--- nihmec/settings/production.py
+++ nihmec/settings/production.py
@@ -4,7 +4,7 @@ from django.core.management.utils import get_random_secret_key
 import dj_database_url
 
 ALLOWED_HOSTS = ["nihmec-production.up.railway.app", "nihmec.com"]
-# DEBUG = False
+DEBUG = False
 
 try:
     from .local import *
